143c16f6c739a23727e6fc82c6ad9aa00df81411
Navgurukul-1 <shweta18@navgurukul.org>
2020-01-11 21:24:35 +0530

    uploading task1

---
+++ task1.py
@@ -0,0 +1,27 @@
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
+URL = "https://www.imdb.com/india/top-rated-indian-movies/"
+sample  = requests.get(URL)
+soup = BeautifulSoup(sample.text,"html.parser")
+tBody = soup.find("tbody",class_="lister-list")
+table_rows = tBody.find_all("tr")
+def scrape_top_list(table_rows):
+	movies_list = []
+	for row in table_rows:
+		dic = {}
+		name = row.find("td", class_ = "titleColumn").a.get_text()
+		position = row.find("td", class_ = "posterColumn").span["data-value"]
+		year = row.find("td", class_= "titleColumn").span.get_text()
+		url = row.find("td",class_="posterColumn").a["href"]
+		movies_link = "https://www.imdb.com/" + url
+		ratings = row.find("td",class_="ratingColumn imdbRating").strong.get_text()
+		dic['position'] = position
+		dic['name'] = name
+		dic['year'] = year
+		dic['ratings'] = ratings
+		dic['url'] = movies_link
+		movies_list.append(dic)
+	return(movies_list)
+
+print(scrape_top_list(table_rows))
